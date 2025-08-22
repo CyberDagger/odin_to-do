9eaba392a557ede12f64516570bc03ed3da6f1c5
CyberDagger <cyberdagger003@gmail.com>
2025-08-22 17:00:42 +0100

    Task class skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./styles.css";
+
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    get title() {
+        return this.title;
+    }
+    set title(title) {
+        this.title = title;
+    }
+    get description() {
+        return this.description;
+    }
+    set description(description) {
+        this.description = description;
+    }
+    get dueDate() {
+        return this.dueDate;
+    }
+    set dueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+    get priority() {
+        return this.priority;
+    }
+    set priority(priority) {
+        this.priority = priority;
+    }
+}

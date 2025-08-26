153b224ac91c4844eff66e9cf9788ce13da12dfd
CyberDagger <cyberdagger003@gmail.com>
2025-08-26 15:34:32 +0100

    Button name sent to event handler

--- src/modules/display.js
+++ src/modules/display.js
@@ -7,9 +7,15 @@ function renderProjects() {
         let item = document.createElement("li");
         let itemButton = document.createElement("button");
         itemButton.textContent = root.projectList[i].name;
+        itemButton.setAttribute("data-name", root.projectList[i].name);
+        itemButton.addEventListener("click", (e) => renderTasks(e.target.getAttribute("data-name")));
         item.appendChild(itemButton);
         projectMenu.appendChild(item);
     }
+}
+
+function renderTasks(project) {
+    alert("You clicked on " + project);
 }
 
 export { renderProjects };

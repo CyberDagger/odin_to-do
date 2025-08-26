8903566b4f7fdb6fd3621250091d325ad50b4622
CyberDagger <cyberdagger003@gmail.com>
2025-08-26 17:42:24 +0100

    Display task cards

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,6 +1,7 @@
 import { root } from "./structure.js";
 
 const projectMenu = document.querySelector("#project-list");
+const taskField = document.querySelector("#content");
 
 function renderProjects() {
     for (let i = 0; i < root.projectList.length; i++) {
@@ -16,7 +17,25 @@ function renderProjects() {
 
 function renderTasks(projectName) {
     let project = root.projectList[root.projectList.map(i => i.name).indexOf(projectName)];
-    alert(project.taskList);
+    for (let i = 0; i < project.taskList.length; i++) {
+        let taskBlock = document.createElement("div");
+        taskBlock.classList.add("task-block");
+        
+        // Title
+        let taskTitle = document.createElement("p");
+        taskTitle.textContent = project.taskList[i].title;
+        taskBlock.appendChild(taskTitle);
+        // Due Date
+        let taskDate = document.createElement("p");
+        taskDate.textContent = project.taskList[i].dueDate;
+        taskBlock.appendChild(taskDate);
+        // Priority
+        let taskPriority = document.createElement("p");
+        taskPriority.textContent = project.taskList[i].priority;
+        taskBlock.appendChild(taskPriority);
+
+        taskField.appendChild(taskBlock);
+    }
 }
 
 export { renderProjects };

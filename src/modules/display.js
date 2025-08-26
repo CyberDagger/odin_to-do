09c740a89a57c201ebc57aece330987e09dd0fc9
CyberDagger <cyberdagger003@gmail.com>
2025-08-26 15:03:27 +0100

    Item is button

--- src/modules/display.js
+++ src/modules/display.js
@@ -5,7 +5,9 @@ const projectMenu = document.querySelector("#project-list");
 function renderProjects() {
     for (let i = 0; i < root.projectList.length; i++) {
         let item = document.createElement("li");
-        item.textContent = root.projectList[i].name;
+        let itemButton = document.createElement("button");
+        itemButton.textContent = root.projectList[i].name;
+        item.appendChild(itemButton);
         projectMenu.appendChild(item);
     }
 }

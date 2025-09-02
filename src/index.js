78a28884150793f9808d23f214c084c0c3b06b85
CyberDagger <cyberdagger003@gmail.com>
2025-09-02 13:00:18 +0100

    Add tasks to projects 2 and 3

--- src/index.js
+++ src/index.js
@@ -15,5 +15,8 @@ console.log(root.projectList);
 root.projectList[0].addTask("Test Task 1", "02/05/2025", "High", "Note 1");
 root.projectList[0].addTask("Test Task 2", "02/05/2025", "High", "Note 2");
 root.projectList[0].addTask("Test Task 3", "02/05/2025", "High", "Note 3");
+root.projectList[1].addTask("Test Task 4", "02/05/2025", "High", "Note 4");
+root.projectList[1].addTask("Test Task 5", "02/05/2025", "High", "Note 5");
+root.projectList[2].addTask("Test Task 6", "02/05/2025", "High", "Note 6");
 
 renderProjects();
